476b60aa3b88a2cbd999dd3814176a0e3a237f7d
minofis <minofis.mail@gmail.com>
2026-07-19 16:22:33 +0300

    feature: add tickets data recieving and displaying in table

--- frontend/src/components/TicketsTable/TicketsTable.tsx
+++ frontend/src/components/TicketsTable/TicketsTable.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './TicketsTable.module.css'
 
 const TicketsTable: React.FC = () => {
+
+  const [tickets, setTickets] = useState<Ticket[]>([])
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      try {
+        const response = await fetch('http://localhost:5220/api/Tickets')
+        const data: Ticket[] = await response.json()
+        setTickets(data)
+        console.log('Received tickets:', data)
+      } catch (error) {
+        console.error('Error fetching tickets:', error)
+      }
+    }
+
+    fetchTickets()
+  }, [])
+
   return (
 <table className={styles.table}>
   <thead>
@@ -15,14 +33,16 @@ const TicketsTable: React.FC = () => {
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td className={styles.cell}></td>
-      <td className={styles.cell}></td>
-      <td className={styles.cell}></td>
-      <td className={styles.cell}></td>
-      <td className={styles.cell}></td>
-      <td className={styles.cell}></td>
-    </tr>
+    {tickets.map((ticket) => (
+      <tr key={ticket.id}>
+        <td className={styles.cell}>{ticket.id}</td>
+        <td className={styles.cell}>{ticket.roomNumber}</td>
+        <td className={styles.cell}>{ticket.authorName}</td>
+        <td className={styles.cell}>{ticket.description}</td>
+        <td className={styles.cell}>{ticket.createdAt}</td>
+        <td className={styles.cell}>{ticket.statusText}</td>
+      </tr>
+    ))}
   </tbody>
 </table>
   )
